faf666b25865ff6115e9f3020dc6039c38cd77a0
Jeorgi Bernard <Jeorgi.Bernard@gmail.com>
2024-10-27 21:47:12 -0600

    added photo carousel to home page and styled the contact page button

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const greeting = document.getElementById("greeting");
 
 greeting.style.textAlign = "center";
@@ -12,3 +13,22 @@ const username = window.prompt("What's your name?", "Please enter your name!");
 greeting.innerHTML = "Welcome to my website, " + username +"!";
 
 
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
